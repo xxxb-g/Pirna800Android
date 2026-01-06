93350c89f35f958265e4af807322ebad6d2dc17b
xxxb-g <xxxbGamer@proton.me>
2026-01-06 19:36:35 +0100

    falscher pfad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("app/keystore.jks")
+            storeFile = file("keystore.jks")
             storePassword = System.getenv("STORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
