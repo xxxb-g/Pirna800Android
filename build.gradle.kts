e54a3a3868a5e587010b387f3c04d61a92281190
xxxb-g <xxxbGamer@proton.me>
2026-01-06 17:55:45 +0100

    build.gradle.kts anpassen
    Signieren muss man da festlegen? Aha...

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,19 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+            keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
+            keyAlias = System.getenv("ANDROID_KEY_ALIAS")
+        }
+    }
+    
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            // ... weitere Release-Konfigurationen
+        }
+    }
+}
